6048b2e90a388307f9f14a856f31c4ce50848956
reymark magsanay <reymark.magsanay@gmail.com>
2026-03-19 18:41:21 +0800

    feat: added function removeFromArray (for loop solution)

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+   let argsArray = [...args] 
+   let result = []
+   for (let i = 0; i < arr.length; i++) {
+    if (argsArray.includes(arr[i]) === false) {
+        result.push(arr[i])
+    }
+   }
+   return result;
 };
 
 // Do not edit below this line
