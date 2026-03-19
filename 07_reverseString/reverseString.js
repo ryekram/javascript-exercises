afe65fb656aeb425e3702a1897741127f07a30f1
reymark magsanay <reymark.magsanay@gmail.com>
2026-03-19 18:14:45 +0800

    feat: added function reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reversed = "";
+    let strArr = str.split("");
+    for (let i = strArr.length - 1; i >= 0; i--) {
+        reversed += strArr[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
