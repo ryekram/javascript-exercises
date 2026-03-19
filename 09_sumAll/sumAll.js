2b4518fb8379e8886cb4b0901c97db4c93f70318
reymark magsanay <reymark.magsanay@gmail.com>
2026-03-19 20:32:41 +0800

    feat: added function sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b))
+    return "ERROR";
+  let start = a > b ? b : a;
+  let end = b < a ? a : b;
+  let result = 0;
+  for (let i = start; i <= end; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
