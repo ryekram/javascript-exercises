62ca4b1948ef3bd64f90b3b67df171a4bac5a33b
ryekram <reymark.magsanay@gmail.com>
2026-04-23 20:07:35 +0800

    Added solution for palindrome

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let cleanStr = str.toLowerCase().replace(/[^a-zA-Z0-9]/g,'')
+    const reverse = cleanStr.split('').reduce((acc, curLet) => curLet + acc, '')
+    if(cleanStr === reverse) return true;
+    return false;
 };
+palindromes('r3ace3car')
+
 
 // Do not edit below this line
 module.exports = palindromes;
